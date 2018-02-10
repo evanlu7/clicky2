48ab1fe55d7ea442d28bb86d6af34dfb12cffc3e
Evan Lu <evanlu7@gmail.com>
2018-02-09 19:04:43 -0800

    formatted files and folders

--- clicky2 react app/src/App.js
+++ clicky2 react app/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from "react";
 import Game from "./components/Game";
+
 const App = () => <Game />;
 
 export default App;
--- clicky2 react app/src/components/Nav/Nav.js
+++ clicky2 react app/src/components/Nav/Nav.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Nav.css";
-import Game from "../Game";
 import NavMessage from "../Nav"
 
 const Nav = props => (
