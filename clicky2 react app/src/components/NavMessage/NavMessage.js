af4adaa5e37b48e5092b7ab3c11b6d9323d8b9ff
Evan Lu <evanlu7@gmail.com>
2018-02-09 18:12:33 -0800

    updated navmessage

--- clicky2 react app/src/components/NavMessage/NavMessage.js
+++ clicky2 react app/src/components/NavMessage/NavMessage.js
@@ -1,6 +1,45 @@
-const Nav = props => (
-<nav className = "navbar">
-<div score = {props.score} highScore={props.highScore} />
-</nav>
-	);
+import React, { Component} from "react";
+import "./NavMessage.css";
 
+class NavMessage extends Component {
+	state = {
+		message: "",
+		animating: false
+	};
+
+componentWillReceiveProps({ score, topScore }) {
+	let newState = { animating: true};
+	if (score === 0 && topScore === 0) {
+		newState.message = "";
+	} else if (score=== 0 && topScore > 0) {
+		newState.message = "incorrect";
+	} else {
+		newState.message = "correct";
+	}
+	this.setState(newState, () =>
+		setTimeout(() => this.setState({ animating: false }), 500)
+
+		);
+}
+
+renderMessage = () => {
+	switch(this.state.message){
+		case "correct":
+			return "You guessed correctly!";
+		case "incorrect":
+			return "You guessed incorrectly!";
+		default:
+			return "Click an image to begin!";
+	}
+};
+
+render () {
+	return (
+		<li className={this.state.animating ? this.state.message: ""}>
+		{this.renderMessage()}
+		</li>
+		);
+}
+}
+
+export default NavMessage;
